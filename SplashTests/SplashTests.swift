30f01c3b8cc879a0ed4a7c45ef871922c09a5549
songkyung <min.songkyung@gmail.com>
2020-04-10 14:36:44 +0900

    intial commit

---
+++ SplashTests/SplashTests.swift
@@ -0,0 +1,34 @@
+//
+//  SplashTests.swift
+//  SplashTests
+//
+//  Created by Running Raccoon on 2020/04/10.
+//  Copyright © 2020 Running Raccoon. All rights reserved.
+//
+
+import XCTest
+@testable import Splash
+
+class SplashTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
